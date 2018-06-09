cb394693c0606a05d7af9fb616cad3f7af687dad
897308902 <897308902@qq.com>
2018-06-09 19:25:08 +0800

    打开自己的页面

---
+++ HelloWorld/Hello/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render_to_response,HttpResponseRedirect
+from django.http import HttpResponse
+
+
+def index(request,id):
+    return HttpResponse(u'<h1>dsfsafa</h1>===%s'%id)
+
+def cheng(request):
+    return render_to_response('first.html')
+# Create your views here.
