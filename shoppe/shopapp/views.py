4533223a9391fc0d28b868bd5e080bdd66fd6586
897308902 <897308902@qq.com>
2018-06-25 18:46:32 +0800

    能增加数据，刷新页面也会提交

--- shoppe/shopapp/views.py
+++ shoppe/shopapp/views.py
@@ -1,30 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render,render_to_response
-from django.template import RequestContext
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext, loader
 from django.http import HttpResponse
-from django.template import loader
 from shopapp.models import Shopp
 import time
+
+
 # Create your views here.
 
 def index(request):
+    return render_to_response('index.html')
 
-	return render_to_response('index.html')
 
 def sales(request):
-	return render_to_response('sales.html')
+    return render_to_response('sales.html')
+
 
 def regist(request):
-	request.encoding = 'utf-8'
-	timestr='00000000'
-	if request.POST:
-		timestr = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+    request.encoding = 'utf-8'
 
-        obj= Shopp(name=request.POST.get('usename'), phone=request.POST.get('phone'), 
-        	ctime=timestr,sex=request.POST.get('sex'))
-        obj.save()
+    if request.POST:
+        timestr = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+        Shopp.objects.create(name=request.POST.get('usename'), phone=request.POST.get('phone'), pwd=request.POST['pwd1'],
+                email=request.POST['email'], ctime=timestr, sex=request.POST.get('sex'))
 
-
-	return render_to_response('regist.html')
+    return render(request, 'regist.html')
