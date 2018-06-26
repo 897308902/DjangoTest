87dd129a06a175bd1ffd75aebf8ea834f8c36312
897308902 <897308902@qq.com>
2018-06-26 18:46:51 +0800

    关联表

--- lotsql/appsql/models.py
+++ lotsql/appsql/models.py
@@ -3,22 +3,23 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
 class Teacher(models.Model):
-	id = models.IntegerField(primary_key=True)
-	name = models.CharField(max_length=50)
-	class Meta:
-		db_table='teacher'
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
 
-		
+    class Meta:
+        db_table = 'teacher'
+
+
 class Student(models.Model):
-	id=models.AutoField(primary_key=True)
-	name = models.CharField(max_length=50)
-	age=models.IntegerField()
-	intine=models.DateTimeField()
-	sex=models.IntegerField()
-	teacher=models.ForeignKey(Teacher,related_name='student_teacher')
-	class Meta:
-		db_table = 'student'
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    intine = models.DateTimeField()
+    sex = models.IntegerField()
+    teacher = models.ForeignKey(Teacher, related_name='teacher', to_field='id')
 
-
+    class Meta:
+        db_table = 'student'
--- lotsql/appsql/views.py
+++ lotsql/appsql/views.py
@@ -4,17 +4,16 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from appsql.models import Student,Teacher
+from appsql.models import Student, Teacher
+
+
 # Create your views here.
 
 def student_list(request):
-	t= loader.get_template('student.html')
-	# studentList=Student.objects.all().order_by('-age')
-	teacher=Teacher.objects.get(id=1)
-	studentList = teacher.student_teacher.all() #默认 student_set，但不要定义外键名
+    t = loader.get_template('student.html')
+    # studentList=Student.objects.all().order_by('-age')
+    teacher = Teacher.objects.get(id=1)
+    studentList = teacher.student_teacher.all()  # 默认 student_set，但不要定义外键名
 
-	c={"studentList":studentList}
-	return HttpResponse(t.render(c))
-
-
-
+    c = {"studentList": studentList}
+    return HttpResponse(t.render(c))
