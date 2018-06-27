29783bf490cb19393e99586408571a9afbd765e9
897308902 <897308902@qq.com>
2018-06-27 18:57:48 +0800

    整理

--- shoppe/shopapp/views.py
+++ shoppe/shopapp/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, render_to_response
 from django.template import RequestContext, loader
 from django.http import HttpResponse
 from shopapp.models import Shopp
+# from . import models
 import time
 
 
