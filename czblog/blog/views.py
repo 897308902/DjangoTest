83bf72e5dd1318ef4642e22be520dcb3402b4042
897308902 <897308902@qq.com>
2018-08-02 19:20:59 +0800

    优化页面显示

--- czblog/blog/views.py
+++ czblog/blog/views.py
@@ -11,11 +11,28 @@ from markdown import markdown
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-# 主页搜索
+# 主页搜索  增加翻页
 def search(request):
     title = request.GET['title']
-    titles = models.Blogs.objects.filter(title__contains=title).order_by('-rcount')
-    return render(request, 'blog/index.html', {'blogs': titles})
+
+    # 增加翻页
+    blogs = models.Blogs.objects.filter(title__contains=title).order_by('-rcount')
+
+    # 生成paginator对象,定义每页显示10条记录
+    paginator = Paginator(blogs, 10)
+    # 从前端获取当前的页码数,默认为1
+    page = request.GET.get('page', 1)
+    # 把当前的页码数转换成整数类型
+    currentPage = int(page)
+
+    try:
+        blogs = paginator.page(currentPage)  # 获取当前页码的记录
+    except PageNotAnInteger:
+        blogs = paginator.page(1)  # 如果用户输入的页码不是整数时,显示第1页的内容
+    except EmptyPage:
+        blogs = paginator.page(paginator.num_pages)  # 如果用户输入的页数不在系统的页码列表中时,显示最后一页的内容
+
+    return render(request, 'blog/index.html', locals())
 
 
 # 我的博客搜索
@@ -24,8 +41,23 @@ def mysearch(request):
         return redirect('/login/')
     name = request.user
     title = request.GET['title']
-    titles = models.Blogs.objects.filter(uname=name, title__contains=title).order_by('-rcount')
-    return render(request, 'blog/userblog.html', {'blogs': titles})
+    # 增加翻页
+    blogs = models.Blogs.objects.filter(uname=name, title__contains=title).order_by('-rcount')
+    # 生成paginator对象,定义每页显示10条记录
+    paginator = Paginator(blogs, 10)
+    # 从前端获取当前的页码数,默认为1
+    page = request.GET.get('page', 1)
+    # 把当前的页码数转换成整数类型
+    currentPage = int(page)
+
+    try:
+        blogs = paginator.page(currentPage)  # 获取当前页码的记录
+    except PageNotAnInteger:
+        blogs = paginator.page(1)  # 如果用户输入的页码不是整数时,显示第1页的内容
+    except EmptyPage:
+        blogs = paginator.page(paginator.num_pages)  # 如果用户输入的页数不在系统的页码列表中时,显示最后一页的内容
+
+    return render(request, 'blog/userblog.html', locals())
 
 
 # 错误页面
