03ac91111f151c8801182cd6d92b2a44df86ff13
897308902 <897308902@qq.com>
2018-08-29 15:43:13 +0800

    commpany

--- czblog/blog/views.py
+++ czblog/blog/views.py
@@ -11,6 +11,30 @@ from markdown import markdown
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
+# 首页
+def index(request):
+    old_url = request.get_full_path()
+    print "index===old_url", old_url
+    blogs = models.Blogs.objects.all().order_by('-rcount')  # [0:10]
+    print blogs
+    # 生成paginator对象,定义每页显示10条记录
+    paginator = Paginator(blogs, 10)
+    # 从前端获取当前的页码数,默认为1
+    page = request.GET.get('page', 1)
+    # 把当前的页码数转换成整数类型
+    currentPage = int(page)
+
+    try:
+        blogs = paginator.page(currentPage)  # 获取当前页码的记录
+    except PageNotAnInteger:
+        blogs = paginator.page(1)  # 如果用户输入的页码不是整数时,显示第1页的内容
+    except EmptyPage:
+        blogs = paginator.page(paginator.num_pages)  # 如果用户输入的页数不在系统的页码列表中时,显示最后一页的内容
+
+    # locals()函数会以字典类型返回当前位置的全部局部变量。
+    return render(request, 'blog/index.html', locals())
+
+
 # 主页搜索  增加翻页        搜索的结果翻页会报错
 def search(request):
     title = request.GET.get('title')
@@ -99,7 +123,7 @@ def ulike(request, blog_id):
 
 
 # 删除自己的评论
-def del_comms(request,blog_id):
+def del_comms(request, blog_id):
     if not request.user.is_authenticated:
         return redirect("/login")
 
@@ -130,27 +154,6 @@ def compage(request, blog_id):
     return render(request, '/blog/%s' % blog_id, locals())
 
 
-# 首页
-def index(request):
-    old_url = request.get_full_path()
-    print "index===old_url", old_url
-    blogs = models.Blogs.objects.all().order_by('-rcount')  # [0:10]
-    # 生成paginator对象,定义每页显示10条记录
-    paginator = Paginator(blogs, 10)
-    # 从前端获取当前的页码数,默认为1
-    page = request.GET.get('page', 1)
-    # 把当前的页码数转换成整数类型
-    currentPage = int(page)
-
-    try:
-        blogs = paginator.page(currentPage)  # 获取当前页码的记录
-    except PageNotAnInteger:
-        blogs = paginator.page(1)  # 如果用户输入的页码不是整数时,显示第1页的内容
-    except EmptyPage:
-        blogs = paginator.page(paginator.num_pages)  # 如果用户输入的页数不在系统的页码列表中时,显示最后一页的内容
-    return render(request, 'blog/index.html', locals())  # {'blogs': blogs}
-
-
 # 按标签
 def marks(request, tags):
     blogs = models.Blogs.objects.filter(marks_id=tags).order_by('-rcount')
@@ -170,81 +173,3 @@ def marks(request, tags):
 
     return render(request, 'blog/marks.html', locals())
 
-
-# 使用user模块注册登录
-
-
-# 修改密码
-def set_pwd(request):
-    if request.method == "POST":
-        oldpassword = request.POST.get("oldpassword")
-        newpassword = request.POST.get("newpassword")
-        # 得到当前登录的用户，判断旧密码是不是和当前的密码一样
-        username = request.user  # 打印的是当前登录的用户名
-        user = User.objects.get(username=username)  # 查看用户
-        ret = user.check_password(oldpassword)  # 检查密码是否正确
-        if ret:
-            user.set_password(newpassword)  # 如果正确就给设置一个新密码
-            user.save()  # 保存
-            return redirect("/login/")
-        else:
-            info = "输入的旧密码不正确"
-            return render(request, "set_pwd.html", {"logs": info})
-    return render(request, "set_pwd.html")
-
-
-# 登录
-def uselogin(request):
-    if request.method == 'GET':
-        # 记住来源的url,如果没有则设置为首页('/blog')
-        request.session['login_from'] = request.META.get('HTTP_REFERER', '/blog')
-    if request.POST:
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        # 输入正确的账号，返回用户名，否则返回none
-        user = authenticate(username=username, password=password)
-        if user:
-            print 66666666666, user
-            login(request, user)
-            # 重定向到来源的url
-            return HttpResponseRedirect(request.session['login_from'])
-            # return redirect('/blog/')
-        else:
-            return render(request, 'login.html', {'logs': '账号或密码错误！%s' % user})
-
-    return render(request, 'login.html', {'logs': ' '})
-
-
-# 注册
-def reg(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        # password2 = request.POST.get("password2")
-        email = request.POST.get("email")
-        # first_name=request.POST.get("first_name")
-        # last_name = request.POST.get("last_name")
-        # if password1 != password2:
-        #     return render(request, "blog/register.html", {'logs': '两次密码出入不一致'})
-        name = User.objects.filter(username=username)
-        # 如果用户存在，则name=1,不存在则name=0
-        if name:
-            return render(request, "register.html", {'message': '用户已存在%s' % len(name)})
-        # else:
-        # return render(request, "regist.html", {'logs': '用户bu存在%s' % len(name)})
-        # 得到用户输入的用户名和密码创建一个新用户
-        User.objects.create_user(username=username, password=password, email=email)
-
-        # 注册成功后自动登录
-        user = authenticate(username=username, password=password)
-        if user:
-            login(request, user)
-            return redirect("/blog/")
-    return render(request, "register.html")
-
-
-# 注销
-def log_out(request):
-    if request.user.is_authenticated:
-        logout(request)
-    return redirect("/blog/")
