473cb7732afb3ee7588471aff910f82c872a7c78
897308902 <897308902@qq.com>
2018-08-29 21:27:47 +0800

    merge

--- czblog/blog/views.py
+++ czblog/blog/views.py
@@ -154,30 +154,6 @@ def compage(request, blog_id):
     return render(request, '/blog/%s' % blog_id, locals())
 
 
-<<<<<<< HEAD
-=======
-# 首页
-def index(request):
-    old_url = request.get_full_path()
-    print "index===old_url", old_url
-    blogs = models.Blogs.objects.all().order_by('-rcount')  # [0:10]
-    # 生成paginator对象,定义每页显示10条记录
-    paginator = Paginator(blogs, 10)
-    # 从前端获取当前的页码数,默认为1
-    page = request.GET.get('page', 1)
-    # 把当前的页码数转换成整数类型
-    currentPage = int(page)
-
-    try:
-        blogs = paginator.page(currentPage)  # 获取当前页码的记录
-    except PageNotAnInteger:
-        blogs = paginator.page(1)  # 如果用户输入的页码不是整数时,显示第1页的内容
-    except EmptyPage:
-        blogs = paginator.page(paginator.num_pages)  # 如果用户输入的页数不在系统的页码列表中时,显示最后一页的内容
-    return render(request, 'blog/index.html', locals())  # {'blogs': blogs} 切换这个就不能获取页数了
-
-
->>>>>>> dev
 # 按标签
 def marks(request, tags):
     blogs = models.Blogs.objects.filter(marks_id=tags).order_by('-rcount')
